5bb9a8b84f23ad28014fdfb8ffef6edfcfd1907c
Leo <leo80103@gmail.com>
2024-04-23 15:12:37 +0800

    準備切版新貼文頁面

---
+++ src/components/CreatePostComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="title position-relative d-flex justify-content-center align-items-center mb-5">
+    <h3>追蹤名單</h3>
+  </section>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
