76dba7c700f1aceb895c7c7240bb4199ccc27143
Leo <leo80103@gmail.com>
2024-05-14 15:00:51 +0800

    架設前端表單驗證成功

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -9,23 +9,90 @@
         <h1 class="logo text-center">MetaWall</h1>
         <h5 class="text-center">到原宇宙展開全新社交圈</h5>
         <br>
-        <form class="d-flex flex-column">
+        <v-form class="d-flex flex-column" v-slot="{ errors }" @submit="onSubmit" >
+            <v-field
+              id="email"
+              name="Email"
+              type="email"
+              class="MetaWall_input my-1"
+              :class="{ 'is-invalid': errors['Email'] }"
+              placeholder="請輸入 Email" rules="email|required"
+              v-model="user.email"
+            ></v-field>
+            <error-message name="Email" class="invalid-feedback"></error-message>
+
+            <v-field
+              id="password"
+              name="密碼"
+              type="password"
+              class="MetaWall_input my-1"
+              :class="{ 'is-invalid': errors['密碼'] }"
+              placeholder="請輸入密碼" rules="min:8|required"
+              v-model="user.password"
+            ></v-field>
+            <error-message name="密碼" class="invalid-feedback"></error-message>
+
+            <button class="btn MetaWall_button btnShdow mt-2" type="submit">登入</button>
+            <router-link class="routerLink" to="signup">
+              註冊帳號
+            </router-link>
+        </v-form>
+
+        <!-- <form class="d-flex flex-column">
           <input class="MetaWall_input my-1" type="email" placeholder="Email">
           <input class="MetaWall_input my-1" type="password" placeholder="Password">
           <button class="btn MetaWall_button btnShdow mt-2" type="button">登入</button>
           <router-link class="routerLink" to="signup">
             註冊帳號
           </router-link>
-        </form>
+        </form> -->
       </div>
     </section>
   </main>
 </template>
 
 <script>
+// 把需要的語系 驗證 驗證規則引入
+import * as VeeValidate from 'vee-validate';
+import * as VeeValidateI18n from '@vee-validate/i18n';
+import * as VeeValidateRules from '@vee-validate/rules';
+
+// 中文語系 JSON
+import zhTW from '../assets/zh_TW.json';
+// 表單驗證規則全部引入使用
+Object.keys(VeeValidateRules).forEach((rule) => {
+  VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+});
+
+// 讀取外部的資源
+VeeValidateI18n.localize({
+  zhTW,
+});
+// Activate the locale
+VeeValidate.configure({
+  generateMessage: VeeValidateI18n.localize('zhTW'),
+  validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
+});
 
 export default {
-
+  components: {
+    VForm: VeeValidate.Form,
+    VField: VeeValidate.Field,
+    ErrorMessage: VeeValidate.ErrorMessage,
+  },
+  data() {
+    return {
+      user: {
+        email: '',
+        password: '',
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.user);
+    },
+  },
 };
 </script>
 
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -9,24 +9,118 @@
         <h1 class="logo text-center">MetaWall</h1>
         <h5 class="text-center">註冊</h5>
         <br>
-        <form class="d-flex flex-column">
-          <input class="MetaWall_input my-1" type="text" placeholder="暱稱">
-          <input class="MetaWall_input my-1" type="email" placeholder="Email">
-          <input class="MetaWall_input my-1" type="password" placeholder="Password">
-          <button class="btn MetaWall_button btnShdow mt-2" type="button">註冊</button>
-          <router-link class="routerLink" to="login">
-            登入
-          </router-link>
-        </form>
+          <v-form class="d-flex flex-column" v-slot="{ errors }" @submit="onSubmit" >
+            <v-field
+              id="name"
+              name="暱稱"
+              type="text"
+              class="MetaWall_input my-1"
+              :class="{ 'is-invalid': errors['暱稱'] }"
+              placeholder="請輸入暱稱" rules="required"
+              v-model="user.name"
+            ></v-field>
+            <error-message name="暱稱" class="invalid-feedback"></error-message>
+
+            <v-field
+              id="email"
+              name="Email"
+              type="email"
+              class="MetaWall_input my-1"
+              :class="{ 'is-invalid': errors['Email'] }"
+              placeholder="請輸入 Email" rules="email|required"
+              v-model="user.email"
+            ></v-field>
+            <error-message name="Email" class="invalid-feedback"></error-message>
+
+            <v-field
+              id="password"
+              name="密碼"
+              type="password"
+              class="MetaWall_input my-1"
+              :class="{ 'is-invalid': errors['密碼'] }"
+              placeholder="請輸入密碼" rules="min:8|required"
+              v-model="user.password"
+            ></v-field>
+            <error-message name="密碼" class="invalid-feedback"></error-message>
+
+            <div>
+              <v-field
+                id="male"
+                name="性別"
+                type="radio"
+                :class="{ 'is-invalid': errors['性別'] }"
+                value="male" rules="required"
+                v-model="user.sex"
+              ></v-field>
+              <label for="male" class="mx-1">男性</label>
+              &emsp;
+              <v-field
+                id="female"
+                name="性別"
+                type="radio"
+                :class="{ 'is-invalid': errors['性別'] }"
+                value="female" rules="required"
+                v-model="user.sex"
+              ></v-field>
+              <label for="female" class="mx-1">女性</label>
+              <error-message name="性別" class="invalid-feedback"></error-message>
+            </div>
+
+            <button class="btn MetaWall_button btnShdow mt-2" type="submit">註冊</button>
+            <router-link class="routerLink" to="login">
+              登入帳號
+            </router-link>
+          </v-form>
       </div>
     </section>
   </main>
 </template>
 
 <script>
+// 把需要的語系 驗證 驗證規則引入
+import * as VeeValidate from 'vee-validate';
+import * as VeeValidateI18n from '@vee-validate/i18n';
+import * as VeeValidateRules from '@vee-validate/rules';
+
+// 中文語系 JSON
+import zhTW from '../assets/zh_TW.json';
+// 表單驗證規則全部引入使用
+Object.keys(VeeValidateRules).forEach((rule) => {
+  VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+});
+
+// 讀取外部的資源
+VeeValidateI18n.localize({
+  zhTW,
+});
+// Activate the locale
+VeeValidate.configure({
+  generateMessage: VeeValidateI18n.localize('zhTW'),
+  validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
+});
 
 export default {
-
+  components: {
+    VForm: VeeValidate.Form,
+    VField: VeeValidate.Field,
+    ErrorMessage: VeeValidate.ErrorMessage,
+  },
+  data() {
+    return {
+      user: {
+        name: '',
+        email: '',
+        sex: '',
+        password: '',
+        photo: '../../public/userPhotoDefault.svg',
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.user);
+    },
+  },
 };
 </script>
 
